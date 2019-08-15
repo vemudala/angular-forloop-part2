952514611eae9dabe2b40256d17d7416f7c044fe
vemudala <maheshvemudala@gmail.com>
2019-08-15 19:24:03 +0530

    Add files via upload

---
+++ itile.ts
@@ -0,0 +1,9 @@
+export interface ITile {  // create interface by "ng generate interface ITile" next add variables and types
+    image:string;
+    name:string;
+    model:string;
+    price:number;
+    rating:number;
+    status:number;
+
+}
